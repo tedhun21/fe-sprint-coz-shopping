279128b0e381de0527343e5c272745072e3db758
tedhun21 <songjihun940409@gmail.com>
2023-07-12 17:00:39 +0900

    feat/axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 import "./App.css";
 import Home from "./page/Home";
@@ -7,6 +9,15 @@ import Bookmark from "./page/Bookmark";
 import Header from "./component/Header";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://cozshopping.codestates-seb.link/api/v1/products?count=4")
+      .then((res) => setProducts(res.data));
+  }, []);
+
+  console.log(products);
   return (
     <section className="wrapper">
       <Header />
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,7 +1,11 @@
+import "./Home.css";
+
 const Home = () => {
   return (
     <>
-      <div>Hi Im home</div>
+      <div></div>
+      <span className="itemList-title">상품 리스트</span>
+      <span className="bookMark-title">북마크 리스트</span>
     </>
   );
 };
